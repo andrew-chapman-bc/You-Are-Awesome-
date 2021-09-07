78190b83be53d0cdca2a3306a451afb296eed135
Andrew Chapman <andrew.chapman@bc.edu>
2021-09-06 23:31:27 -0400

    Added up until 2.12

---
+++ ButtonsAndStacks/ButtonsAndStacks/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ButtonsAndStacks
+//
+//  Created by Andrew Chapman on 9/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    var colors: [UIColor] = [.red,
+                             .orange,
+                             .yellow,
+                             .green,
+                             .blue,
+                             .systemIndigo,
+                             .systemPurple]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        print("You clicked tag number: \(sender.tag)")
+        
+        if sender.tag >= 0 && sender.tag <= 6 {
+            colorLabel.text = "You clicked \(sender.currentTitle!)"
+            colorLabel.textColor = colors[sender.tag]
+        } else {
+            colorLabel.text = ""
+        }
+        
+//        if sender.tag == 1000{
+//            colorLabel.text = ""
+//        } else {
+//            colorLabel.text = "You clicked \(sender.currentTitle!)"
+//            colorLabel.textColor = colors[sender.tag]
+//        }
+        
+    }
+    
+}
+
--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,17 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imageNumber = 0
+    var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("👍 viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's You!"
+        messageLabel.text = ""
     }
 
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
-        messageLabel.text = "You Are Awesome!"
+        
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "Fabulous? That's You!",
+                        "You've Got The Design Skills of Jony Ive"]
+        
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber >= messages.count{
+//            messageNumber = 0
+//        }
+        
+        //let imageName = "image" + String(imageNumber)
+        //let imageName = "image\(imageNumber)"
+//        imageView.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
+//        }
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if  messageLabel.text == greatMessage {
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
+        
     }
 }
 
